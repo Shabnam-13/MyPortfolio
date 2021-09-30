0c77b72d49eb05909638202bf1bea5dffb724d55
Shabnam Hasanli <57615674+Shabnam-13@users.noreply.github.com>
2021-09-30 11:53:20 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// Loader function
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(".mainPage").classList.add('d-none');
+      document.querySelector(".loader").classList.remove('d-none');
+  } else {
+      document.querySelector(".loader").classList.add('d-none');
+      document.querySelector(".mainPage").classList.remove('d-none');
+  }
+};
+
 // Slide Animation JS codesd
 var links = document.querySelectorAll(".navbar-nav .nav-item .nav-link");
 var sections = document.querySelectorAll(".sections");
@@ -11,14 +22,17 @@ links.forEach(link => {
     for(var i = 0; i < sections.length; i++){
       var sectionName = sections[i].getAttribute("id");
       if(nav == sectionName){
-        document.body.style.overflow = "hidden";
+        document.body.style.cssText = "overflow:hidden; background-color:#111";
         document.getElementById("home").classList.add('d-none');
         slideBg.classList.add('slideRight');
-        sections[i].classList.remove('d-none');
+        setTimeout(function(){
+          document.getElementById(sectionName).classList.remove('d-none');
+        },800)
         setTimeout(function(){
             slideBg.classList.remove('slideRight'); 
-            document.body.style.overflow = "visible";
-        }, 1500);
+            document.body.style.cssText = "overflow:visible; background-color:unset";
+          }, 1500);
+          return;
       }
     }
   })
@@ -26,14 +40,42 @@ links.forEach(link => {
 
 closeBtn.forEach(btn => {
   btn.addEventListener('click',function(){
-    document.body.style.overflow = "hidden";
+    document.body.style.cssText = "overflow:hidden; background-color:#111";
     var sectionName = btn.parentElement.getAttribute("id");
     document.getElementById(sectionName).classList.add("d-none");
-    document.getElementById("home").classList.remove("d-none");
     slideBg.classList.add('slideLeft');
     setTimeout(function(){
+      document.getElementById("home").classList.remove("d-none");
+    },800)
+    setTimeout(function(){
         slideBg.classList.remove('slideLeft');  
-        document.body.style.overflow = "visible";
+        document.body.style.cssText = "overflow:visible; background-color:unset";
     }, 1500);
+  })
+});
+
+// Projects
+var titles = document.querySelectorAll(".projectTitle ul li a");
+var projects = document.querySelectorAll(".project");
+
+titles.forEach(title => {
+  title.addEventListener('click',function(e){
+    e.preventDefault();
+    var titleName = title.getAttribute('href').slice(1);
+    for(var i = 0; i < projects.length; i++){
+      var activeTitle = document.querySelector(".projectTitle ul li.active");
+      if(activeTitle != null){
+        activeTitle.classList.remove('active');
+      }
+      if(titleName == projects[i].getAttribute("id")){
+        projects[i].classList.remove('d-none');
+      }else{
+        projects[i].classList.add('d-none');
+      }
+      if(titleName == "allProjects"){
+        projects[i].classList.remove('d-none');
+      }
+    }
+    title.parentElement.classList.add('active');
   })
 });
